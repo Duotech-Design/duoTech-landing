98209061957af38e4036433fd5f2816f517efbbe
RA-1124 <alison.rangelperez@gmail.com>
2024-04-24 14:56:25 -0600

    cambio servicios a nuestros servicios

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -56,7 +56,7 @@ export const Features = () => {
       className="container py-24 sm:py-32 space-y-8"
     >
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-        NUESTROS {" "}
+       CONOCE NUESTROS {" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
           SERVICIOS
         </span>
