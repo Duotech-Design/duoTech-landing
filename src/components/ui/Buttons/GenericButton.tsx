dc8187366641c5b7eeee2fc1622d5687b379d7cb
RA-1124 <alison.rangelperez@gmail.com>
2024-05-14 21:13:11 -0600

    change button style

--- src/components/ui/Buttons/GenericButton.tsx
+++ src/components/ui/Buttons/GenericButton.tsx
@@ -1,14 +1,15 @@
 
 
 
-
+//" bg-[#2D2E31 text-white font-bold py-3 px-10 rounded-full  hover:bg-[#879EAD]
 
 
 const GenericButton = () => {
   return (
-    <button className="bg-[#2D2E31] hover:bg-[#879EAD] text-white font-bold py-3 px-10 rounded-full">
-      Cotiza tu proyecto
-    </button>
+    <button className="bg-transparent text-[#73726C] font-semibold hover:text-[#879EAD] py-2 px-4 border border-[#73726C] hover:border-[#879EAD] rounded-full">
+    Cotiza tu proyecto 
+  </button>
+  
   );
 };
 
