98e08505ad6bc1e2a85c78190fef11d5fc60ca7a
RA-1124 <alison.rangelperez@gmail.com>
2024-05-17 16:14:24 -0600

    SMALL CORRECTIONS IN TEXT

--- src/components/Servicios.tsx
+++ src/components/Servicios.tsx
@@ -56,7 +56,7 @@ export const Servicios = () => {
   return (
     <section id="servicios" className="container py-24 sm:py-32 space-y-8">
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-       Construyes negocios, juntos los {" "}
+       Construye negocios, juntos los {" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
           transformamos.
         </span>
