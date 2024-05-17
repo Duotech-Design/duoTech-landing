e344935d48f0cc3cfeeb0c6c7b6af18320a44455
RA-1124 <alison.rangelperez@gmail.com>
2024-05-17 13:22:59 -0600

    changes in style motion in generic button

--- src/components/ui/Buttons/GenericButton.tsx
+++ src/components/ui/Buttons/GenericButton.tsx
@@ -1,18 +1,24 @@
 
 
-
+import { motion } from "framer-motion";
 
 //" bg-[#2D2E31 text-white font-bold py-3 px-10 rounded-full  hover:bg-[#879EAD]
 
 
 const GenericButton = () => {
   return (
-    <button className="bg-transparent flex justify-center text-[#73726C] font-semibold hover:text-[#879EAD] py-2 px-4 border border-[#73726C] hover:border-[#879EAD] rounded-full">
-    Cotiza tu proyecto <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <motion.button
+    whileHover={{ scale : 1.2}}
+    className="bg-transparent flex justify-center text-[#73726C]  font-semibold hover:text-[#879EAD] py-2 px-4 border border-[#73726C] hover:border-[#879EAD] rounded-full"
+    >
+   
+    Cotiza tu proyecto
+     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 
 <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
-  </button>
+</motion.button>
+
   
   );
 };
