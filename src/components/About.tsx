faf274a4a8dc1478091ac09fc7436c9feb5b4c75
RA-1124 <alison.rangelperez@gmail.com>
2024-05-18 13:45:04 -0600

    add image to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,33 +1,10 @@
-import { Statistics } from "./Statistics";
-import pilot from "../assets/pilot.png";
+import heroImage from "../../public/heroImage.png";
 
 export const About = () => {
   return (
-    <section
-      id="about"
-      className="container py-24 sm:py-32"
-    >
-      <div className="bg-muted/50 border rounded-lg py-12">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
-          <img
-            src={pilot}
-            alt=""
-            className="w-[300px] object-contain rounded-lg"
-          />
-          <div className="bg-green-0 flex flex-col justify-between">
-            <div className="pb-6">
-              <h2 className="text-3xl md:text-4xl font-bold">
-               
-              Nosotros
-              </h2>
-              <p className="text-xl text-muted-foreground mt-4">
-              Nos especializamos en diseño web y desarrollo de soluciones digitales a medida, enfocándonos exclusivamente en la creación de páginas web. Además, construimos todo el ecosistema de tu marca para mejorar la percepción y experiencia de tu audiencia, diferenciándote de la competencia, aumentando la satisfacción del usuario, fomentando la lealtad del cliente y fortaleciendo el valor de tu marca.
-              </p>
-            </div>
-
-            <Statistics />
-          </div>
-        </div>
+    <section id="about" className="container py-24 sm:py-32">
+      <div className=" flex justify-center mt-10">
+        <img src={heroImage} className="object-cover h-[600px] w-auto "></img>
       </div>
     </section>
   );
