e37b7cd0bb3d0ef024d0eed5950fa402c5f79945
RA-1124 <alison.rangelperez@gmail.com>
2024-05-17 21:12:39 -0600

    CHANGE THE TEXT TO THE MIDDLE

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,22 +6,21 @@ import GenericButton from "./ui/Buttons/GenericButton";
 
 export const Hero = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10 ">
+    <section className="container grid place-items-center py-20 md:py-32 gap-10 ">
       <div className="flex flex-col justify-center items-center h-full">
-        <main className="text-center lg:text-start space-y-6">
+        <main className="text-center space-y-6">
           <h1 className="text-5xl md:text-6xl font-bold">
             <span className="inline bg-gradient-to-r from-[#d4d3d4]  to-[#879EAD] text-transparent bg-clip-text">
               TECNOLOGIA 
             </span>{" "}
             
-          </h1>{" "}
-          &{" "}
-          <h2 className="inline">
+          </h1>
+          <h1 className="text-5xl md:text-6xl font-bold">
             <span className="inline bg-gradient-to-r from-[#d4d3d4] via-[#879EAD] to-[#879EAD] text-transparent bg-clip-text">
-              DISEÑO
+             &  DISEÑO
             </span>{" "}
             
-          </h2>
+          </h1>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
