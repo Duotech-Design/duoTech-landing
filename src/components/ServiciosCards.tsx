eae3f9bd12a848c8ca86b5dbd097c792fab81f0f
RA-1124 <alison.rangelperez@gmail.com>
2024-05-23 12:41:56 -0600

    CARDS WITH EFFECT

--- src/components/ServiciosCards.tsx
+++ src/components/ServiciosCards.tsx
@@ -1,91 +1,77 @@
-import {
-  Card2,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card2";
-//import image from "../assets/growth.png";
-import image3 from "../assets/reflecting.png";
-import image4 from "../assets/looking-ahead.png";
-import GenericButton2 from "./ui/Buttons/GenericButton2";
 
-//import { buttonVariants } from "./ui/button";
+import { motion } from "framer-motion";
 
+import imageCard2 from "../../public/imageCard2.png"
+import imageCard from "../../public/imageCard.png"
+//import GenericButton2 from "./ui/Buttons/GenericButton2";
+
+// Interface de propiedades de servicios
 interface ServiciosProps {
   title: string;
   description: string;
-
   image: string;
 }
 
+// Definición de servicios
 const features: ServiciosProps[] = [
   {
-    title: "One page website",
+    title: "ONE PAGE WEBSITE",
     description:
-      "Una pag. con efecto parallax. Propuesta de diseño. Diseño responsivo (Desktop, tablet y celular). Formulario de contacto personalizado. Secciones personalizadas (links a redes sociales, contenido de la empresa, imagenes, servicios, info. de contacto, codigo QR de tu pag, etc.). Links de Whatsapp.  ",
-    image: image4,
+      " La primera impresión es crucial.  Nuestro servicio de one page web garantiza una presentación impactante y efectiva desde el primer vistazo.",
+    image: imageCard,
   },
   {
-    title: "Páginas Web Administrables",
+    title: "LANDING PAGE",
     description:
-      "Pag. con 10 secciones. Propuesta de diseño. Diseño responsivo (Desktop, tablet y celular). Formulario de contacto personalizado. Secciones personalizadas (links a redes sociales, contenido de la empresa, imagenes, servicios, info. de contacto, codigo QR de tu pag, etc.). Links de Whatsapp. ",
-    image: image3,
+      "Proporcionamos a tu negocio un lenguaje gráfico unificado y auténtico que conecta con tu audiencia en todos los puntos de contacto.",
+    image: imageCard2,
   },
-  /*{
-    title: "Páginas Web Empresariales",
-    description:
-      "Ofrecemos servicios de páginas web empresariales diseñadas para optimizar procesos según las necesidades de cada negocio. Nuestro enfoque es crear páginas que ayuden a eficientizar operaciones internas y mejorar la productividad de la empresa, todo de manera personalizada para adaptarse a cada requerimiento específico.",
-    image: image,
-  },*/
+ 
 ];
-{
-  /* 
-const serviciosList: string[] = [
-  "Responsivos",
-  "Personalizados",
-  "Eficaces",
-  /*"E-commerce",
-  "Optimización SEO",
-  "Dinamico",
 
-  "Minimalist",
-];
-*/
-}
 export const ServiciosCards = () => {
   return (
     <section id="servicios" className="container py-6 space-y-8">
-      <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-        Escoge la mejor opcion para tu negocio.
+      <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0 text-center">
+        Escoge la mejor opción para tu negocio.
       </p>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          className="w-full"
-          src="/imageCard.png"
-          alt="One page website"
-        />
-
-        <div className="px-6 py-4">
-         
-          <p className="text-gray-700 text-base">
-          La primera impresión es crucial.  Nuestro servicio de one page web garantiza una presentación impactante y efectiva desde el primer vistazo.
-          </p>
-          <div className="font-bold text-xl mb-2">ONE PAGE WEBSITE</div>
-        </div>
-       
+      <div className="flex justify-center space-x-8">
+        {features.map((feature, index) => (
+          <motion.div
+            key={index}
+            className="max-w-sm rounded overflow-hidden shadow-lg"
+            whileHover={{ scale: 1.05 }}
+          >
+            <img className="w-full" src={feature.image} alt={feature.title} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{feature.title}</div>
+              <p className="text-gray-700 text-base">{feature.description}</p>
+            </div>
+          </motion.div>
+        ))}
       </div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-  <img className="w-full" src="/imageCard2.png" alt="Landing page"/>
-  <div className="px-6 py-4">
-   
-    <p className="text-gray-700 text-base">
-    Proporcionamos a tu negocio un lenguaje gráfico unificado y auténtico que conecta con tu audiencia en todos los puntos de contacto.
-    </p>
-    <div className="font-bold text-xl mb-2">LANDING PAGE</div>
-  </div>
- 
-</div>
     </section>
   );
 };
+
+// Código del componente principal
+import { StrictMode, useState } from "react";
+import * as ReactDOMClient from "react-dom/client";
+
+function AppWithUI() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <StrictMode>
+      <App key={count} />
+      <Footer
+        title="Press/tap animations"
+        url="https://www.framer.com/docs/gestures/#hover"
+      />
+    </StrictMode>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<AppWithUI />);
