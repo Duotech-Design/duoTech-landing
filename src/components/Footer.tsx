13b96388192b56e6d6ee01699f0c6745cdb970fc
RA-1124 <alison.rangelperez@gmail.com>
2024-05-29 12:50:08 -0600

    changes in footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
-import { LogoIcon } from "./Icons";
-
 export const Footer = () => {
   return (
-    <footer id="footer" >
+    <footer id="footer">
       <hr className="w-11/12 mx-auto" />
 
-      <section className="container py-20 grid grid-cols-1 md:grid-cols-4 gap-x-12 gap-y-8">
+      <section className="container py-20 grid grid-cols-1 md:grid-cols-3 gap-x-12 gap-y-8">
         <div className="flex flex-col justify-between">
           <div>
             <h3 className="text-xs text-gray-400">STATUS</h3>
@@ -23,46 +21,50 @@ export const Footer = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="text-xs text-gray-400">CONTACTO</h3>
-          <div>
-            <a href="mailto:design@duotech.com" className="opacity-60 hover:opacity-100">
-              DESIGN@DUOTECH.COM
-            </a>
-          </div>
-          <div>
-            <a href="tel:+51444" className="opacity-60 hover:opacity-100">
-              +51 444 ------
-            </a>
-          </div>
-          <div>
-            <a href="tel:+5244" className="opacity-60 hover:opacity-100">
-              44- --------
-            </a>
-          </div>
-          <div>
-            <a href="mailto:__@duptechdesign.com" className="opacity-60 hover:opacity-100">
-              __@duptechdesign.com
-            </a>
-          </div>
-        </div>
+        <div className="md:ml-auto md:col-span-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12">
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xs text-gray-400">CONTACTO</h3>
+              <div>
+                <a href="mailto:design@duotech.com" className="opacity-60 hover:opacity-100">
+                  DESIGN@DUOTECH.COM
+                </a>
+              </div>
+              <div>
+                <a href="tel:+51444" className="opacity-60 hover:opacity-100">
+                  +51 444 ------
+                </a>
+              </div>
+              <div>
+                <a href="tel:+5244" className="opacity-60 hover:opacity-100">
+                  44- --------
+                </a>
+              </div>
+              <div>
+                <a href="mailto:__@duptechdesign.com" className="opacity-60 hover:opacity-100">
+                  __@duotechdesign.com
+                </a>
+              </div>
+            </div>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="text-xs text-gray-400">LINKS</h3>
-          <div>
-            <a href="#" className="opacity-60 hover:opacity-100">
-              INSTAGRAM
-            </a>
-          </div>
-          <div>
-            <a href="#" className="opacity-60 hover:opacity-100">
-              PINTEREST
-            </a>
-          </div>
-          <div>
-            <a href="#" className="opacity-60 hover:opacity-100">
-              DUO-TECH SHOP
-            </a>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xs text-gray-400">LINKS</h3>
+              <div>
+                <a href="#" className="opacity-60 hover:opacity-100">
+                  INSTAGRAM
+                </a>
+              </div>
+              <div>
+                <a href="#" className="opacity-60 hover:opacity-100">
+                  PINTEREST
+                </a>
+              </div>
+              <div>
+                <a href="#" className="opacity-60 hover:opacity-100">
+                  DUO-TECH SHOP
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -72,7 +74,7 @@ export const Footer = () => {
           &copy; 2024 Landing page made by{" "}
           <a
             target="_blank"
-            href="https://github.com/leoMirandaa"
+            href="duotechdesign.com"
             className="text-primary transition-all border-primary hover:border-b-2"
           >
             DuoTech Design
