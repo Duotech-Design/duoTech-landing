b97987cb88e5c65f7658738609f3739d4b54c055
RA-1124 <alison.rangelperez@gmail.com>
2024-05-30 15:26:14 -0600

    NEW ANIMATION TO CONTACT SECTION

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import "/Users/alisonrangel/Desktop/RA/proyectos /duoTech-landing/src/index.css"; // Asegúrate de importar el archivo de estilos
+
 export const Contacto = () => {
   return (
     <section id="contacto" className="container py-24 sm:py-32">
       <div className="flex flex-col items-center justify-center mt-6">
         <p className="text-sm">SI BUSCAS ALGO ...</p>
-        <h2 className="text-5xl md:text-6xl font-bold my-3">NUEVO</h2>
+        <h2 className="text-5xl md:text-6xl font-bold my-3">
+          <span className="animated-text"></span>
+        </h2>
         <div className="flex items-center mt-4">
           <p className="text-sm mr-4">EMPECEMOS A</p>
           <span className="px-6 py-2 border border-black rounded-full text-black">
@@ -14,3 +19,5 @@ export const Contacto = () => {
     </section>
   );
 };
+
+// styles.css
