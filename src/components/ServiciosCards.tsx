1f5995f93b44c34d48f7a4784598356ce4c927b0
RA-1124 <alison.rangelperez@gmail.com>
2024-06-05 12:27:34 -0600

    info to services cards back

--- src/components/ServiciosCards.tsx
+++ src/components/ServiciosCards.tsx
@@ -1,5 +1,6 @@
 import imageCard2 from "/imageCard2.png";
 import imageCard from "/imageCard.png";
+import GenericButton from "./ui/Buttons/GenericButton";
 
 // Interface de propiedades de servicios
 interface ServiciosProps {
@@ -24,6 +25,55 @@ const features: ServiciosProps[] = [
   },
 ];
 
+// Interface de propiedades de cotización
+interface CotizacionProps {
+  title: string;
+  popular: number;
+  price: number;
+  description: string;
+  buttonText: string;
+  benefitList: string[];
+}
+
+const pricingList: CotizacionProps[] = [
+  {
+    title: "ONE PAGE WEBSITE",
+    popular: 0,
+    price: 0,
+    description:
+      "Una pag. con efecto parallax. Solución web eficiente que condensa toda la información esencial de tu negocio en una única página, ideal para proyectos y promociones puntuales, ofreciendo una experiencia de navegación directa y concisa para tus visitantes.",
+    buttonText: "Get Started",
+    benefitList: [
+      "Propuesta de diseño.",
+      "Diseño responsivo (Desktop, tablet y mobile).",
+      "Hasta 4 paginas",
+      "Formulario de contacto personalizado.",
+      "Secciones personalizadas.",
+      "Links a redes sociales",
+      "Links de Whatsapp.",
+      "Codigo QR de tu pag,"
+    ],
+  },
+  {
+    title: "LANDING PAGE",
+    popular: 1,
+    price: 5,
+    description:
+      "Diseño web enfocado en captar la atención y convertir visitantes en clientes potenciales, optimizando la experiencia del usuario y destacando la propuesta de valor de tu negocio en una sola página.",
+    buttonText: "Start Free Trial",
+    benefitList: [
+      "Pag. con 10 secciones.",
+      "Propuesta de diseño.",
+      "Diseño responsivo (Desktop, tablet y mobile).",
+      "Formulario de contacto personalizado.",
+      "Secciones personalizadas",
+      "Links a redes sociales",
+      "Links de Whatsapp.",
+      "Codigo QR de tu pag,"
+    ],
+  },
+];
+
 export const ServiciosCards = () => {
   return (
     <section id="servicios" className="container py-6 space-y-8">
@@ -31,22 +81,32 @@ export const ServiciosCards = () => {
         Escoge la mejor opción para tu negocio.
       </p>
       <div className="flex justify-center space-x-8">
-        {features.map((feature, index) => (
-          <div key={index} className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front bg-white">
-                <img className="w-full h-64 object-cover rounded-t-lg" src={feature.image} alt={feature.title} />
-                <div className="px-6 py-4 text-left">
-                  <div className="font-bold text-xl mb-2">{feature.title}</div>
-                  <p className="text-gray-700 roboto-Font">{feature.description}</p>
+        {features.map((feature, index) => {
+          const pricing = pricingList.find(item => item.title.toUpperCase() === feature.title);
+          return (
+            <div key={index} className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front bg-white">
+                  <img className="w-full h-64 object-cover rounded-t-lg" src={feature.image} alt={feature.title} />
+                  <div className="px-6 py-4 text-left">
+                    <div className="font-bold text-xl mb-2">{feature.title}</div>
+                    <p className="text-gray-700 roboto-Font">{feature.description}</p>
+                  </div>
+                </div>
+                <div className="flip-card-back p-6 flex flex-col justify-center items-center bg-gray-800 text-white rounded-lg">
+                  <h1 className="text-xl font-bold mb-4">{pricing?.title}</h1>
+                  <p className="mb-4">{pricing?.description}</p>
+                  <ul className="list-disc list-inside mb-4">
+                    {pricing?.benefitList.map((benefit, idx) => (
+                      <li key={idx}>{benefit}</li>
+                    ))}
+                  </ul>
+                  < GenericButton title="Contáctanos" />
                 </div>
               </div>
-              <div className="flip-card-back p-6 flex justify-center items-center">
-                <h1 className="text-xl font-bold text-white">{feature.title}</h1>
-              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
