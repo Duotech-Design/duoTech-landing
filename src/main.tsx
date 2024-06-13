ba2601728049208e6649053bcdb3a1df0b200055
ZoiloMendoza <zoilomm@hotmail.com>
2024-06-13 03:30:12 -0600

    set I18

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next"; 
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
+    </I18nextProvider>
   </React.StrictMode>
 );
