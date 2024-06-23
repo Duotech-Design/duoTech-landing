3cf1a8866be92777e4a31097c1ae37f4a35c0280
cupenizmm <cupenizmm@gmail.com>
2024-06-23 22:08:33 +0000

    op

--- src/components/ServiciosCards.tsx
+++ src/components/ServiciosCards.tsx
@@ -10,24 +10,6 @@ interface ServiciosProps {
 }
 
 // Definición de servicios
-<<<<<<< HEAD
-const features: ServiciosProps[] = [
-  {
-    title: "ONE PAGE WEBSITE",
-    description:
-      "Una página web con efecto parallax que presenta toda la información esencial de tu negocio en una sola página. Ideal para proyectos y promociones puntuales, ofrece una navegación directa y concisa.",
-    image: imageCard,
-  },
-  {
-    title: "LANDING PAGE",
-    description:
-      "Proporcionamos a tu negocio un lenguaje gráfico unificado y auténtico que conecta con tu audiencia en todos los puntos de contacto.",
-    image: imageCard2,
-  },
-];
-=======
-
->>>>>>> 9b877ac (form)
 
 // Interface de propiedades de cotización
 interface CotizacionProps {
@@ -39,48 +21,6 @@ interface CotizacionProps {
   benefitList: string[];
 }
 
-<<<<<<< HEAD
-const pricingList: CotizacionProps[] = [
-  {
-    title: "ONE PAGE WEBSITE",
-    popular: 0,
-    price: 0,
-    description:
-      "Una página web con efecto parallax que presenta toda la información esencial de tu negocio en una sola página. Ideal para proyectos y promociones puntuales, ofrece una navegación directa y concisa.",
-    buttonText: "Get Started",
-    benefitList: [
-      "Propuesta de diseño",
-      "Diseño responsivo (Desktop, tablet y móvil)",
-      "Hasta 4 páginas",
-      "Formulario de contacto personalizado",
-      "Secciones personalizadas",
-      "Links a redes sociales",
-      "Links de WhatsApp",
-      "Código QR de tu página",
-    ],
-  },
-  {
-    title: "LANDING PAGE",
-    popular: 1,
-    price: 5,
-    description:
-      "Diseño web enfocado en captar la atención y convertir visitantes en clientes potenciales, optimizando la experiencia del usuario y destacando la propuesta de valor de tu negocio en una sola página.",
-    buttonText: "Start Free Trial",
-    benefitList: [
-      "Página con 10 secciones",
-      "Propuesta de diseño",
-      "Diseño responsivo (Desktop, tablet y móvil)",
-      "Formulario de contacto personalizado",
-      "Secciones personalizadas",
-      "Links a redes sociales",
-      "Links de WhatsApp",
-      "Código QR de tu página",
-    ],
-  },
-];
-
-=======
->>>>>>> 9b877ac (form)
 export const ServiciosCards = () => {
   const { t } = useTranslation("global");
   const pricingList: CotizacionProps[] = [
@@ -135,40 +75,21 @@ export const ServiciosCards = () => {
     },
   ];
   return (
-<<<<<<< HEAD
-    <section id="servicios" className="container py-8 space-y-8">
-      <p className="text-xl text-muted-foreground mx-auto lg:mx-0 text-center roboto-Font mb-8">
-        Escoge la mejor opción para tu negocio.
-      </p>
-      <div className="flex flex-wrap justify-center space-x-8 space-y-8 lg:space-y-0 lg:flex-nowrap">
-=======
     <section id="servicios" className="container py-8 space-y-8 h-[1000px]">
       <p className="text-lg md:text-xl text-muted-foreground mx-auto lg:mx-0 text-center roboto-Font mb-8">
       {t("services_cards.title")}
       </p>
       <div className="flex flex-col md:flex-row md:justify-center md:space-x-8 space-y-4 md:space-y-0">
->>>>>>> 9b877ac (form)
         {features.map((feature, index) => {
           const pricing = pricingList.find(
             (item) => item.title.toUpperCase() === feature.title
           );
           return (
-<<<<<<< HEAD
-            <div
-              key={index}
-              className="flip-card w-full lg:w-1/3"
-            >
-              <div className="flip-card-inner">
-                <div className="flip-card-front bg-white">
-                  <img
-                    className="contrast-50 w-full h-48 lg:h-96 object-cover rounded-t-lg"
-=======
             <div key={index} className="flip-card w-full h-[450px] md:h-[600px] md:w-1/2">
               <div className="flip-card-inner">
                 <div className="flip-card-front bg-white">
                   <img
                     className="contrast-50 w-full h-48 md:h-[450px] object-cover rounded-t-lg"
->>>>>>> 9b877ac (form)
                     src={feature.image}
                     alt={feature.title}
                   />
