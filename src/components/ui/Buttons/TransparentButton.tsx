23ac5df072a4d6dacf769d7000ff62df2b067fa0
cupenizmm <cupenizmm@gmail.com>
2024-06-28 08:05:25 +0000

    update form

--- src/components/ui/Buttons/TransparentButton.tsx
+++ src/components/ui/Buttons/TransparentButton.tsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import MessengerHover from "@/components/MessengerHover";
 
-const TransparentButton = ({ title }: { title: string }) => {
+const TransparentButton = ({ title, state }: { title: string, state: string }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <motion.button
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="bg-transparent flex justify-center items-center text-white 
-        font-semibold transition-text-color duration-300 hover:text-[#879EAD] 
-        py-2 px-2 md:px-8 border border-white hover:border-[#879EAD] rounded-full 
-        md:text-lg text-xs transition-transform duration-300 hover:scale-110 cotiza-button"
+      className={`${state} flex justify-center items-center text-white 
+        font-semibold transition-text-color duration-300 hover:text-${state} 
+        py-2 px-2 md:px-8 border border-white hover:border-${state} rounded-full 
+        md:text-lg text-xs transition-transform duration-300 hover:scale-110 cotiza-button`}
     >
       <MessengerHover text={title} isHovered={isHovered} />
       <svg
