612a2de9cabc02c35e8a8ea9e61dc4f56cddc770
RA-1124 <alison.rangelperez@gmail.com>
2024-07-09 20:59:01 -0600

    carrusel cards

--- src/components/ServiciosCards.tsx
+++ src/components/ServiciosCards.tsx
@@ -1,9 +1,10 @@
-import  { useState } from "react";
+import  { useState, useRef } from "react";
 import imageCard2 from "/imageCard2.jpg";
 import imageCard from "/imageCard.png";
 import GenericButton2 from "./ui/Buttons/GenericButton2";
 import { useTranslation } from "react-i18next";
 import { Cotiza } from "./Cotiza";
+import { motion, useScroll } from "framer-motion";
 
 // Interface de propiedades de servicios
 interface ServiciosProps {
@@ -23,6 +24,8 @@ interface CotizacionProps {
 }
 
 export const ServiciosCards = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollXProgress } = useScroll({ container: ref });
   const { t } = useTranslation("global");
   const [showCotiza, setShowCotiza] = useState(false);
 
@@ -70,10 +73,54 @@ export const ServiciosCards = () => {
         t("services_cards.card_2.benefistList.benefit_7"),
         t("services_cards.card_2.benefistList.benefit_8"),
       ],
+    }, {
+      title: "ONE PAGE WEBSITE",
+      popular: 0,
+      price: 0,
+      description: t("services_cards.card_1.description"),
+      buttonText: "Get Started",
+      benefitList: [
+        t("services_cards.card_1.benefistList.benefit_1"),
+        t("services_cards.card_1.benefistList.benefit_2"),
+        t("services_cards.card_1.benefistList.benefit_3"),
+        t("services_cards.card_1.benefistList.benefit_4"),
+        t("services_cards.card_1.benefistList.benefit_5"),
+        t("services_cards.card_1.benefistList.benefit_6"),
+        t("services_cards.card_1.benefistList.benefit_7"),
+        t("services_cards.card_1.benefistList.benefit_8"),
+      ],
+    },
+    {
+      title: "LANDING PAGE",
+      popular: 1,
+      price: 5,
+      description: t("services_cards.card_2.description"),
+      buttonText: "Get Started",
+      benefitList: [
+        t("services_cards.card_2.benefistList.benefit_1"),
+        t("services_cards.card_2.benefistList.benefit_2"),
+        t("services_cards.card_2.benefistList.benefit_3"),
+        t("services_cards.card_2.benefistList.benefit_4"),
+        t("services_cards.card_2.benefistList.benefit_5"),
+        t("services_cards.card_2.benefistList.benefit_6"),
+        t("services_cards.card_2.benefistList.benefit_7"),
+        t("services_cards.card_2.benefistList.benefit_8"),
+      ],
     },
   ];
 
   const features: ServiciosProps[] = [
+    {
+      title: "ONE PAGE WEBSITE",
+      description: t("services_cards.card_1.description_front_card"),
+      image: imageCard,
+    },
+    {
+      title: "LANDING PAGE",
+      description:
+      t("services_cards.card_2.description_front_card"),
+      image: imageCard2,
+    },
     {
       title: "ONE PAGE WEBSITE",
       description: t("services_cards.card_1.description_front_card"),
@@ -98,13 +145,13 @@ export const ServiciosCards = () => {
         <p className="text-lg md:text-xl text-muted-foreground mx-auto lg:mx-0 text-center roboto-Font mb-8">
           {t("services_cards.title")}
         </p>
-        <div className="flex flex-col md:flex-row md:justify-center md:space-x-8 space-y-4 md:space-y-0">
+        <div ref={ref} className="lista flex flex-col md:flex-row md:justify-center md:space-x-8 space-y-4 md:space-y-0">
           {features.map((feature, index) => {
             const pricing = pricingList.find(
               (item) => item.title.toUpperCase() === feature.title
             );
             return (
-              <div key={index} className="flip-card w-full h-[450px] md:h-[600px] md:w-1/2">
+              <div key={index} className="listaHijo flip-card w-full h-[450px] md:h-[600px] md:w-1/2">
                 <div className="flip-card-inner group">
                   <div className="flip-card-front bg-white">
                     <img
