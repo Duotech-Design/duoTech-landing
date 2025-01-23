ef423787bcf5eca98d7d28403a7228af4b2b6887
root <root@LAPTOP-SQL44NUG.localdomain>
2025-01-22 23:33:55 -0600

    Página en Construcción

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,44 @@
-import { About } from "./components/About";
-//import { FAQ } from "./components/Contacto";
-import { Servicios } from "./components/Servicios";
-import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
-import { Navbar } from "./components/Navbar";
-import { ScrollToTop } from "./components/ScrollToTop";
-import "./App.css";
-import { Contacto } from "./components/Contacto";
 //import { Cotiza } from "./components/Cotiza";
-import { Us } from "./components/Us";
-import { ServiciosCards } from "./components/ServiciosCards";
-import { Proyectos } from "./components/Proyectos";
-import { useInViewport } from "./lazyLoadedComponent/useInViewport";
+//import { FAQ } from "./components/Contacto";
+//+++++++++++++++++++++++++++++++++++++++++++++
+// import { About } from "./components/About";
+// import { Servicios } from "./components/Servicios";
+// import { Footer } from "./components/Footer";
+// import { Hero } from "./components/Hero";
+// import { Navbar } from "./components/Navbar";
+// import { ScrollToTop } from "./components/ScrollToTop";
+import "./App.css";
+// import { Contacto } from "./components/Contacto";
+// import { Us } from "./components/Us";
+// import { ServiciosCards } from "./components/ServiciosCards";
+// import { Proyectos } from "./components/Proyectos";
+// import { useInViewport } from "./lazyLoadedComponent/useInViewport";
 
 function App() {
-  const { isInViewport, ref } = useInViewport();
+  //const { isInViewport, ref } = useInViewport();
   return (
     <>
-      <Navbar />
+      <div className="container mx-auto flex min-h-screen w-full items-center justify-center bg-transparent">
+        <div className="space-y-4 text-center">
+          <h1 className="text-2xl font-semibold text-gray-700">
+            ¡Página en Construcción!
+          </h1>
+          <p className="text-gray-500">
+            Estamos trabajando para traer algo increíble. ¡Vuelve pronto!
+          </p>
+        </div>
+      </div>
+      {/* <Navbar />
       <Hero />
-      <About />
-      <div ref={ref}>{isInViewport && <Us />}</div>
+      <About /> */}
+      {/* <div ref={ref}>{isInViewport && <Us />}</div> */}
       {/*<HowItWorks /> */}
-      <Servicios />
+      {/* <Servicios />
       <div id="a"></div>
       <ServiciosCards />
       <div id="b"></div>
       <Proyectos />
-      <div id="c"></div>
+      <div id="c"></div> */}
       {/*
       <Cotiza />
       <Services />
@@ -37,9 +48,9 @@ function App() {
     */}
       {/* <Team />*/}
       {/*<Newsletter />*/}
-      <Contacto />
+      {/* <Contacto />
       <Footer />
-      <ScrollToTop />
+      <ScrollToTop /> */}
     </>
   );
 }
